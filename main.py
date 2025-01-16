b0c32b1f666be73ef8c93530453eb797887d1cee
niko86 <git@niko86.com>
2025-01-16 21:49:14 +0000

    trialing open_url into AGS4

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-import arrr
-import matplotlib.pyplot as plt
+import polars as pl
+from python_ags4 import AGS4
+from pyodide.http import open_url
 from pyscript import document, display
 
+url = r"https://gitlab.com/ags-data-format-wg/ags-python-library/-/raw/main/notebooks/East%20West%20Rail%20BGS%20Pre%20October%202018%20upload%20(partial).ags"
+# url = r"East West Rail BGS Pre October 2018 upload (partial).ags"
 
-def translate_english(event):
-    input_text = document.querySelector("#english")
-    english = input_text.value
-    output_div = document.querySelector("#output")
-    output_div.innerText = arrr.translate(english)
-    
-    # Create the plot
-    fig, ax = plt.subplots()
-    ax.plot([1, 2, 3, 4])
-    display(fig, target="plot")
+ags_output = document.querySelector("#output")
+
+tables, headings = AGS4.AGS4_to_dataframe(open_url(url))
+ags_output.innerHTML = str(tables["PROJ"].head())

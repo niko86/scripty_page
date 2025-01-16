cede239e56ea1762e144156e5de8e48a31187ea8
niko86 <git@niko86.com>
2025-01-16 22:08:45 +0000

    test

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-import polars as pl
-from python_ags4 import AGS4
+import io
+
 from pyodide.http import open_url
-from pyscript import document, display
+from pyscript import document
 
 url = r"https://gitlab.com/ags-data-format-wg/ags-python-library/-/raw/main/notebooks/East%20West%20Rail%20BGS%20Pre%20October%202018%20upload%20(partial).ags"
 # url = r"East West Rail BGS Pre October 2018 upload (partial).ags"
 
-ags_output = document.querySelector("#output")
 
-tables, headings = AGS4.AGS4_to_dataframe(open_url(url))
-ags_output.innerHTML = str(tables["PROJ"].head())
+# Fetch the content from the URL
+response = open_url(url)
+
+# Read the content into a buffer
+buffer = io.StringIO(response.read())
+
+ags_output = document.querySelector("#output")
+ags_output.innerHTML = buffer.readlines()

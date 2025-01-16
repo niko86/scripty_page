9c7d1dc0685430ea339c9d96f7844847d88a986f
niko86 <git@niko86.com>
2025-01-16 20:17:28 +0000

    second try

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import io
+import base64
 
 import arrr
 import matplotlib.pyplot as plt
@@ -9,6 +11,19 @@ def translate_english(event):
     english = input_text.value
     output_div = document.querySelector("#output")
     output_div.innerText = arrr.translate(english)
-    plot_div = document.querySelector("#plot")
+    
+    # Create the plot
     plt.plot([1, 2, 3, 4])
-    plot_div.innerHTML = plt.show()
+    
+    # Save the plot to a buffer
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png')
+    buf.seek(0)
+    
+    # Convert the buffer to a base64 string
+    img_base64 = base64.b64encode(buf.read()).decode('utf-8')
+    buf.close()
+    
+    # Set the src attribute of the img element
+    plot_div = document.querySelector("#plot")
+    plot_div.innerHTML = f'<img src="data:image/png;base64,{img_base64}" />'

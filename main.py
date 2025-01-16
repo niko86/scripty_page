d4d673be9faa5f1f891840f31136033a2f120972
niko86 <git@niko86.com>
2025-01-16 20:30:11 +0000

    using display from pyscript

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import io
-import base64
-
 import arrr
 import matplotlib.pyplot as plt
-from pyscript import document
+from pyscript import document, display
 
 
 def translate_english(event):
@@ -13,17 +10,4 @@ def translate_english(event):
     output_div.innerText = arrr.translate(english)
     
     # Create the plot
-    plt.plot([1, 2, 3, 4])
-    
-    # Save the plot to a buffer
-    buf = io.BytesIO()
-    plt.savefig(buf, format='png')
-    buf.seek(0)
-    
-    # Convert the buffer to a base64 string
-    img_base64 = base64.b64encode(buf.read()).decode('utf-8')
-    buf.close()
-    
-    # Set the src attribute of the img element
-    plot_div = document.querySelector("#plot")
-    plot_div.innerHTML = f'<img src="data:image/png;base64,{img_base64}" />'
+    display(plt.plot([1, 2, 3, 4]), target="plot")

e0b1951baf11739527b331b0d468c32522fc676b
niko86 <git@niko86.com>
2025-01-17 10:15:16 +0000

    placing some deps in the code due to CORS issue

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import io
 
+import polars as pl
 from pyodide.http import open_url
 from pyscript import document
 
-url = r"https://gitlab.com/ags-data-format-wg/ags-python-library/-/raw/main/notebooks/East%20West%20Rail%20BGS%20Pre%20October%202018%20upload%20(partial).ags"
+url = r"https://raw.githubusercontent.com/niko86/scripty_page/refs/heads/master/East%20West%20Rail%20BGS%20Pre%20October%202018%20upload%20(partial).ags"
 # url = r"East West Rail BGS Pre October 2018 upload (partial).ags"
 
 
@@ -14,6 +15,7 @@ response = open_url(url)
 buffer = io.StringIO(response.read())
 
 ags_output = document.querySelector("#output")
-ags_output.innerHTML = buffer.readlines()
+ags_output.innerHTML = buffer.readline()
+
 # "https://github.com/pola-rs/polars/"
 # https://raw.githubusercontent.com/pola-rs/polars/releases/download/py-1.20.0/polars-1.20.0-cp39-abi3-emscripten_3_1_58_wasm32.whl

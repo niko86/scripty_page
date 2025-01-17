d6ce97a5c593ea02c518e28456ce93f63dee7c41
niko86 <git@niko86.com>
2025-01-17 17:23:01 +0000

    upload file functionality

---
+++ README.md
@@ -0,0 +1,2 @@
+
+"https://raw.githubusercontent.com/niko86/scripty_page/refs/heads/master/polars-1.20.0-cp39-abi3-emscripten_3_1_58_wasm32.whl"
--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 import io
+import warnings
+warnings.simplefilter(action='ignore', category=DeprecationWarning)
 
-import polars as pl
-from pyodide.http import open_url
-from pyodide.ffi import JsProxy
-from pyscript import document
+import pandas as pd
+import panel as pn
 
-url = r"https://raw.githubusercontent.com/niko86/scripty_page/refs/heads/master/East%20West%20Rail%20BGS%20Pre%20October%202018%20upload%20(partial).ags"
-#url = r"https://gitlab.com/ags-data-format-wg/ags-python-library/-/raw/main/notebooks/East%20West%20Rail%20BGS%20Pre%20October%202018%20upload%20(partial).ags"
-url_output = document.querySelector("#url")
-url_output.innerHTML = f"Ready to load AGS from: \n{url}"
+pn.extension()
 
-def load_ags(event: JsProxy) -> None:
-    # Fetch the content from the URL
-    response = open_url(url)
-    # Read the content into a buffer
-    buffer = io.StringIO(response.read())
+def uploaded_ags(uploaded_data) -> None:
+    if uploaded_data is not None:
+        io_buffer = io.BytesIO(uploaded_data)
+        df = pd.read_csv(io_buffer, skiprows=1, nrows=3)
+        pn.Row(pn.pane.DataFrame(df, width=400)).servable(target="output")
 
-    ags_output = document.querySelector("#output")
-    # Print the first line of the buffer
-    url_output.innerHTML = f"File loaded"
-    ags_output.innerHTML = buffer.readlines()
-    
+file_upload = pn.widgets.FileInput(accept='.ags', multiple=False)
+load_ags_bind = pn.bind(uploaded_ags, file_upload)
+
+(
+    pn.Row(
+        file_upload,
+        load_ags_bind,
+    ).servable(target="url")
+)

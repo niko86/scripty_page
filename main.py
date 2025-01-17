7c5dda0b4e5f0fc0b7f72a61fc60088535bd2629
niko86 <git@niko86.com>
2025-01-17 10:32:57 +0000

    one more little test

--- main.py
+++ main.py
@@ -2,20 +2,22 @@ import io
 
 import polars as pl
 from pyodide.http import open_url
+from pyodide.ffi import JsProxy
 from pyscript import document
 
 url = r"https://raw.githubusercontent.com/niko86/scripty_page/refs/heads/master/East%20West%20Rail%20BGS%20Pre%20October%202018%20upload%20(partial).ags"
-# url = r"East West Rail BGS Pre October 2018 upload (partial).ags"
+url_output = document.querySelector("#url")
+url_output.innerHTML = f"Ready to load AGS from: {url}"
 
+def load_ags(event: JsProxy) -> None:
+    print(type(event))
+    # Fetch the content from the URL
+    response = open_url(url)
+    # Read the content into a buffer
+    buffer = io.StringIO(response.read())
 
-# Fetch the content from the URL
-response = open_url(url)
-
-# Read the content into a buffer
-buffer = io.StringIO(response.read())
-
-ags_output = document.querySelector("#output")
-ags_output.innerHTML = buffer.readline()
-
-# "https://github.com/pola-rs/polars/"
-# https://raw.githubusercontent.com/pola-rs/polars/releases/download/py-1.20.0/polars-1.20.0-cp39-abi3-emscripten_3_1_58_wasm32.whl
+    ags_output = document.querySelector("#output")
+    # Print the first line of the buffer
+    url_output.innerHTML = f"File loaded"
+    ags_output.innerHTML = buffer.readlines()
+    

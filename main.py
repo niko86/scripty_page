76524c1c0c25c62403f786e36a5a143df6658574
niko86 <git@niko86.com>
2025-01-17 18:08:59 +0000

    using tabulator js for dataframe display

--- main.py
+++ main.py
@@ -2,23 +2,26 @@ import io
 import warnings
 warnings.simplefilter(action='ignore', category=DeprecationWarning)
 
-import pandas as pd
 import panel as pn
+from python_ags4 import AGS4
 
-pn.extension()
+pn.extension('tabulator')
 
 def uploaded_ags(uploaded_data) -> None:
     if uploaded_data is not None:
         io_buffer = io.BytesIO(uploaded_data)
-        df = pd.read_csv(io_buffer, skiprows=1, nrows=3)
-        pn.Row(pn.pane.DataFrame(df)).servable(target="output")
+        tables, _ = AGS4.AGS4_to_dataframe(io_buffer)
+        df = pn.widgets.Tabulator(tables['PROJ'])
+        pn.Row(df).servable(target="output")
 
 file_upload = pn.widgets.FileInput(accept='.ags', multiple=False)
+#select_box = pn.widgets.Select(name='Select')
 load_ags_bind = pn.bind(uploaded_ags, file_upload)
-
+# selection_bind = pn.bind(display_ags_data, load_ags_bind, select_box)
 (
     pn.Row(
         file_upload,
+        #select_box,
         load_ags_bind,
     ).servable(target="url")
 )

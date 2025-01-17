8ebb8d9e2f756cee0eb6b80b19fa4e69300c7031
niko86 <git@niko86.com>
2025-01-17 21:43:40 +0000

    minor changes

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import io
 import warnings
-warnings.simplefilter(action='ignore', category=DeprecationWarning)
+warnings.simplefilter(action="ignore", category=DeprecationWarning)
 
 import pandas as pl
 import panel as pn
+from panel.theme import Bootstrap
 from python_ags4 import AGS4
 
-pn.extension('tabulator')
+pn.extension("tabulator", design=Bootstrap)
 
-select_box = pn.widgets.Select(name='Select Table', options=[])
-file_upload = pn.widgets.FileInput(accept='.ags', multiple=False)
+select_box = pn.widgets.Select(name="Select Table", options=[])
+file_upload = pn.widgets.FileInput(accept=".ags", multiple=False)
 
 def uploaded_ags(uploaded_data) -> dict:
     """Load AGS file and update the select widget."""
@@ -27,16 +28,16 @@ def display_ags_data(tables: dict[str, pl.DataFrame], table_name):
     if not tables or table_name not in tables:
         return pn.pane.Markdown("No table selected")
     df = AGS4.convert_to_numeric(tables[table_name].drop("HEADING", axis=1))
-    return pn.widgets.Tabulator(df)
+    tabulator = pn.widgets.Tabulator(df, show_index=False, pagination="remote", page_size=15)
+    return tabulator
 
 # Bind the file upload and selection to display the correct DataFrame
 load_ags_bind = pn.bind(uploaded_ags, file_upload)
 selection_bind = pn.bind(display_ags_data, load_ags_bind, select_box)
 
-output = pn.Column(
+pn.Column(
     pn.Row(file_upload),
     pn.Row(select_box),
-    pn.Row(selection_bind), width_policy='max'
+    pn.Row(selection_bind), 
+    width_policy="max",
 ).servable(target="output")
-
-pn.Column(output)#.servable()
